e9cfb028eb2b70a4014bda026032a561bcf4559b
TomsProject <44722363+TomsProject@users.noreply.github.com>
2026-05-17 22:38:15 +0800

    add hugeicons

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from 'react';
+import { Camera01Icon } from 'hugeicons-react';
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,9 +9,12 @@ function App() {
     <>
       <section id="center">
         <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+<Camera01Icon 
+                size={48} 
+                color="#2F88FF" 
+                strokeWidth={2} // 加粗线条
+                style={{ cursor: 'pointer' }}
+            />
         </div>
         <div>
           <h1>Get started</h1>
@@ -42,13 +43,7 @@ function App() {
           <ul>
             <li>
               <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
+           
                 Learn more
               </a>
             </li>
